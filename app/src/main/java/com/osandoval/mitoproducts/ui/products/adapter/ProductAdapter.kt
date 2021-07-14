f6ed8d078100197ef191436594aa37725581e60b
Orlando Sandoval <orlando.sandoval@gmail.com>
2021-07-13 20:22:08 -0400

    products recyclerview implementation

---
+++ app/src/main/java/com/osandoval/mitoproducts/ui/products/adapter/ProductAdapter.kt
@@ -0,0 +1,51 @@
+package com.osandoval.mitoproducts.ui.products.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.osandoval.mitoproducts.core.BaseViewHolder
+import com.osandoval.mitoproducts.data.model.ProductEntity
+import com.osandoval.mitoproducts.databinding.FragmentProductsItemBinding
+
+class ProductAdapter(private val list: List<ProductEntity>, private val itemClickListener: IOnItemClickListener) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    interface IOnItemClickListener{
+        fun onItemClick(product: ProductEntity)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+      val viewBinding = FragmentProductsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+      val holder = ProductViewHolder(viewBinding, parent.context)
+
+        viewBinding.root.setOnClickListener{
+            val position = holder.bindingAdapterPosition.takeIf { it!= DiffUtil.DiffResult.NO_POSITION }
+                ?: return@setOnClickListener
+            itemClickListener.onItemClick(list[position])
+        }
+
+      return holder
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when(holder){
+            is ProductViewHolder -> holder.bind(list[position])
+        }
+
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    private inner class ProductViewHolder(val binding: FragmentProductsItemBinding, val context: Context)
+        : BaseViewHolder<ProductEntity>(binding.root) {
+
+        override fun bind(item: ProductEntity) {
+           Glide.with(context).load(item.urlImage).centerCrop().into(binding.imageProduct)
+           binding.textViewName.text = item.name
+        }
+
+    }
+
+}
--- app/src/main/java/com/osandoval/mitoproducts/ui/products/fragments/ProductsFragment.kt
+++ app/src/main/java/com/osandoval/mitoproducts/ui/products/fragments/ProductsFragment.kt
@@ -5,21 +5,22 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import com.osandoval.mitoproducts.R
 import com.osandoval.mitoproducts.core.Resource
 import com.osandoval.mitoproducts.data.local.AppDatabase
 import com.osandoval.mitoproducts.data.local.LocalProductDataSource
+import com.osandoval.mitoproducts.data.model.ProductEntity
 import com.osandoval.mitoproducts.data.remote.RemoteProductDataSource
 import com.osandoval.mitoproducts.data.remote.RetrofitClient
 import com.osandoval.mitoproducts.databinding.FragmentProductsBinding
 import com.osandoval.mitoproducts.domain.product.ProductRepository
+import com.osandoval.mitoproducts.ui.products.adapter.ProductAdapter
 import com.osandoval.mitoproducts.ui.products.viewmodel.ProductViewModelFactory
 import com.osandoval.mitoproducts.ui.products.viewmodel.ProductsViewModel
-import java.util.*
-import androidx.lifecycle.Observer
 
 
-class ProductsFragment : Fragment(R.layout.fragment_products) {
+class ProductsFragment : Fragment(R.layout.fragment_products), ProductAdapter.IOnItemClickListener {
     private val TAG ="Meh"
     private lateinit var binding: FragmentProductsBinding
     private val viewModel by viewModels<ProductsViewModel> {
@@ -36,18 +37,23 @@ class ProductsFragment : Fragment(R.layout.fragment_products) {
 
         binding = FragmentProductsBinding.bind(view)
 
-        viewModel.getProducts().observe(viewLifecycleOwner, Observer {
-            when(it) {
+        viewModel.getProducts().observe(viewLifecycleOwner, {result->
+            when(result) {
                 is Resource.Loading -> {
                     Log.d(TAG, "onViewCreated: LOADING...")
                 }
                 is Resource.Success -> {
-                    Log.d(TAG, "onViewCreated: ${it.data}")
+                    binding.recyclerViewProducts.layoutManager = GridLayoutManager(context,2)
+                    binding.recyclerViewProducts.adapter = ProductAdapter(result.data, this@ProductsFragment)
                 }
                 is Resource.Failure -> {
-                    Log.d(TAG, "onViewCreated: ${it.exception}")
+                    Log.d(TAG, "onViewCreated: ${result.exception}")
                 }
             }
         })
+    }
+
+    override fun onItemClick(product: ProductEntity) {
+        Log.d(TAG, "onItemClick:$product")
     }
 }
